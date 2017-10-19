0dad4cec174a81a72d6049317651e96c765c1a96
AswIyer <anaveen@navsim.com>
2017-10-19 12:01:24 -0230

    Voting page added

---
+++ Assignment 2/client/src/src/app/pages/vote/vote.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+	selector: 'app-login',
+	templateUrl: './create-election.component.html',
+	styleUrls: ['./create-election.component.scss']
+})
+export class CreateElectionComponent implements OnInit {
+
+	constructor() {
+
+	}
+
+	ngOnInit() {
+	}
+
+
+}
