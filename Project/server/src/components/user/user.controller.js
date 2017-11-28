2f75bff95f54e822aa28de3792ebf3bc91898e77
Haris Barki <harisbarki@ymail.com>
2017-11-28 14:40:53 -0330

    made user.controller backward compatible with es5.1

--- Project/server/src/components/user/user.controller.js
+++ Project/server/src/components/user/user.controller.js
@@ -5,7 +5,7 @@ const Jwt = require('jsonwebtoken');
 const User = require('./user.model');
 const privateKey = Config.key.privateKey;
 
-exports.create = (req, res) => {
+exports.create = function (req, res) {
 	req.body.password = UserService.encrypt(req.body.password);
 
 	// regex for email test
@@ -14,48 +14,48 @@ exports.create = (req, res) => {
 		user.createdAt = new Date();
 		user.modifiedAt = new Date();
 		user.lastActiveAt = new Date();
-		User.create(user).then((user) => {
+		User.create(user).then(function (user) {
 			res.status(200).json(generateUserJson(user));
-		}).catch((err) => {
+		}).catch(function (err) {
 			console.error(err);
 			var errorResponse = {
 				statusCode: 500,
-				message: `Oh uh, something went wrong`
+				message: "Oh uh, something went wrong"
 			};
 			if (err.name === 'ValidationError') {
 				errorResponse.statusCode = 409;
-				errorResponse.message = `Please provide another email`;
+				errorResponse.message = "Please provide another email";
 			}
 			res.status(errorResponse.statusCode).json(errorResponse)
 		});
 	} else {
 		var errorResponse = {
 			statusCode: 412,
-			message: `Email not valid`
+			message: "Email not valid"
 		};
 		res.status(errorResponse.statusCode).json(errorResponse);
 	}
 };
 
-exports.createIfNotExists = (req, res) => {
+exports.createIfNotExists = function (req, res) {
 	// regex for email test
 	if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(req.body.email)) {
 		var user = req.body;
-		User.findByEmail(user.email).then((user) => {
+		User.findByEmail(user.email).then(function (user) {
 			res.status(200).json(generateUserJson(user));
-		}).catch((err) => {
+		}).catch(function (err) {
 			// if doesnt exists then create a new one with password test
 			user.password = UserService.encrypt('test'); //default password
 			user.createdAt = new Date();
 			user.modifiedAt = new Date();
 
-			User.create(user).then((user) => {
+			User.create(user).then(function (user) {
 				res.status(200).json(generateUserJson(user));
-			}).catch((err) => {
+			}).catch(function (err) {
 				console.error(err);
 				var errorResponse = {
 					statusCode: 500,
-					message: `Oh uh, something went wrong`
+					message: "Oh uh, something went wrong"
 				};
 				res.status(errorResponse.statusCode).json(errorResponse)
 			});
@@ -64,16 +64,16 @@ exports.createIfNotExists = (req, res) => {
 	else {
 		var errorResponse = {
 			statusCode: 412,
-			message: `Email not valid`
+			message: "Email not valid"
 		};
 		res.status(errorResponse.statusCode).json(errorResponse);
 	}
 };
 
-exports.login = (req, res) => {
-	User.findByEmail(req.body.email).then((user) => {
+exports.login = function (req, res) {
+	User.findByEmail(req.body.email).then(function (user) {
 		if (user === null) {
-			res.status(422).json({statusCode: 422, message: `Email not recognised`});
+			res.status(422).json({statusCode: 422, message: "Email not recognised"});
 		} else {
 			if (req.body.password === UserService.decrypt(user.password)) {
 				res.status(200).json(generateUserJson(user));
@@ -81,29 +81,29 @@ exports.login = (req, res) => {
 				// update last login here
 				updateLoginTime(user._id);
 			} else {
-				var errorResponse = {statusCode: 500, message: `Oh uh, something went wrong`};
+				var errorResponse = {statusCode: 500, message: "Oh uh, something went wrong"};
 				res.status(errorResponse.statusCode).json(errorResponse);
 			}
 		}
-	}).catch((err) => {
+	}).catch(function (err) {
 		console.error(err);
-		var errorResponse = {statusCode: 500, message: `Oh uh, something went wrong`};
+		var errorResponse = {statusCode: 500, message: "Oh uh, something went wrong"};
 		res.status(errorResponse.statusCode).json(errorResponse);
 	});
 };
 
-exports.update = (req, res) => {
+exports.update = function (req, res) {
 	var newUser = req.body;
-	Jwt.verify(newUser.token, privateKey, (err, decoded) => {
+	Jwt.verify(newUser.token, privateKey, function (err, decoded) {
 		if (err) {
 			console.error(err);
-			var errorResponse = {statusCode: 500, message: `Oh uh, something went wrong`};
+			var errorResponse = {statusCode: 500, message: "Oh uh, something went wrong"};
 			res.status(errorResponse.statusCode).json(errorResponse);
 		}
 		else {
-			User.findById(decoded.id).then((user) => {
+			User.findById(decoded.id).then(function (user) {
 				if (user === null) {
-					res.status(422).send(`Email not recognised`);
+					res.status(422).send("Email not recognised");
 				}
 				else {
 					// if the email was hacked at frontend, return it to original
@@ -113,32 +113,32 @@ exports.update = (req, res) => {
 						newUser.password = UserService.encrypt(newUser.password);
 					}
 					newUser.modifiedAt = new Date();
-					User.update(newUser).then((updatedUser) => {
+					User.update(newUser).then(function (updatedUser) {
 						res.status(200).json(generateUserJson(updatedUser));
-					}).catch((err) => {
+					}).catch(function (err) {
 						console.error(err);
-						var errorResponse = {statusCode: 500, message: `Oh uh, something went wrong`};
+						var errorResponse = {statusCode: 500, message: "Oh uh, something went wrong"};
 						res.status(errorResponse.statusCode).json(errorResponse);
 					});
 				}
-			}).catch((err) => {
+			}).catch(function (err) {
 				console.error('updateErr', err);
-				var errorResponse = {statusCode: 500, message: `Oh uh, something went wrong`};
+				var errorResponse = {statusCode: 500, message: "Oh uh, something went wrong"};
 				res.status(errorResponse.statusCode).json(errorResponse);
 			});
 		}
 	})
 };
 
-var updateLoginTime = (_id) => {
-	User.findById(_id).then((user) => {
+var updateLoginTime = function (_id) {
+	User.findById(_id).then(function (user) {
 		user.lastActiveAt = new Date();
-		User.update(user).then((updatedUser) => {
+		User.update(user).then(function (updatedUser) {
 		});
 	});
 };
 
-var generateUserJson = (user) => {
+var generateUserJson = function (user) {
 	var tokenData = {
 		email: user.email,
 		id: user._id
