9f4ce43e7e176ecd4170cf0d51317f32719c83ee
Haris Barki <harisbarki@ymail.com>
2017-11-28 22:28:54 -0330

    updated frontend create/edit election page with new data model

--- Project/client/src/app/pages/election-create-edit/election-create-edit.component.ts
+++ Project/client/src/app/pages/election-create-edit/election-create-edit.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 
-import {Election, ElectionService} from '../../services';
+import {Election, ElectionService, User, UserService, PartyHead, District, Voter} from '../../services';
 import {Subscription} from 'rxjs/Subscription';
 
 @Component({
@@ -17,13 +17,19 @@ export class ElectionCreateEditComponent implements OnInit, OnDestroy {
 	errorMessage: string;
 	paramSubscription: Subscription;
 
-	constructor(private router: Router, private activatedRoute: ActivatedRoute, private electionService: ElectionService) {
+	constructor(private router: Router,
+	            private activatedRoute: ActivatedRoute,
+	            private electionService: ElectionService,
+	            private userService: UserService) {
 		this.election = new Election();
 		this.election.winningStrategy = 'winnerTakesAll';
 		this.election.candidatesStrategy = 'nomination';
+		this.election.resultsStrategy = 'liveResults';
 		const currentDate = new Date();
 		this.election.dateFrom = new Date(currentDate.toDateString());
 		this.election.dateTo = new Date(currentDate.toDateString());
+		this.election.nominationCloseDate = new Date(currentDate.toDateString());
+		this.election.resultsReleaseDate = new Date(currentDate.toDateString());
 		this.errorMessage = '';
 	}
 
@@ -33,6 +39,15 @@ export class ElectionCreateEditComponent implements OnInit, OnDestroy {
 			if (params['electionId']) {
 				this.loadingData = true;
 				this.electionService.findById(params['electionId']).then((election: Election) => {
+					// get the tempVoters
+					const tempVoters = [];
+					for (let i = 0; i < election.districts.length; i++) {
+						for (let j = 0; j < election.districts[i].voters.length; j++) {
+							tempVoters.push(election.districts[i].voters[j]._id.email);
+						}
+						election.districts[i].tempVoters = tempVoters.join(', ');
+					}
+
 					this.election = election;
 					this.editElectionMode = true;
 					this.loadingData = false;
@@ -49,17 +64,29 @@ export class ElectionCreateEditComponent implements OnInit, OnDestroy {
 		this.paramSubscription.unsubscribe();
 	}
 
-	onSubmit(election) {
+	async onSubmit() {
 		this.errorMessage = '';
-		if (this.election.dateFrom > this.election.dateTo) {
-			this.errorMessage = 'Starting date of election can not be after end date!!';
+		if (this.election.dateFrom > this.election.dateTo && this.election.nominationCloseDate > this.election.dateTo) {
+			this.errorMessage = 'Starting date/nomination closing date of election can not be after end date!!';
 		} else {
-			// console.log(this.election);
+			this.loadingData = true;
+
+			// add partyHeads and voters
+			this.election = await this.addVotersAndPartyHeads(this.election);
+
+			console.log('added voters and partyHeads', this.election);
+
 			if (this.editElectionMode) {
 				this.election.dateFrom = new Date(this.election.dateFrom);
 				this.election.dateTo = new Date(this.election.dateTo);
+				this.election.nominationCloseDate = new Date(this.election.nominationCloseDate);
+				if (this.election.resultsStrategy === 'selectedDate') {
+					this.election.resultsReleaseDate = new Date(this.election.resultsReleaseDate);
+				} else {
+					this.election.resultsReleaseDate = null;
+				}
 				console.log('Updating election', this.election);
-				this.electionService.update(this.election).then(() => {
+				await this.electionService.update(this.election).then(() => {
 					this.router.navigate([this.electionService.urls.dashboard()]);
 				}).catch((err) => {
 					console.error(err);
@@ -67,17 +94,85 @@ export class ElectionCreateEditComponent implements OnInit, OnDestroy {
 				});
 			} else {
 				console.log('Creating election', this.election);
-				this.electionService.create(this.election).then(() => {
+				await this.electionService.create(this.election).then(() => {
 					this.router.navigate([this.electionService.urls.dashboard()]);
 				}).catch((err) => {
 					console.error(err);
 					this.errorMessage = err;
 				});
 			}
+			// this should run after everything is done because of async await
+			this.loadingData = false;
 		}
-		// console.log('election', election);
-		// console.log('this.election', this.election);
+	}
+
+	addEmptyPartyHead() {
+		this.election.partyHeads.push(new PartyHead);
+	}
+
+	addEmptyDistrict() {
+		this.election.districts.push(new District);
 	}
 
 
+	async addVotersAndPartyHeads(election: Election) {
+		// party heads
+		for (let i = 0; i < election.partyHeads.length; i++) {
+			const user = new User();
+			user.email = election.partyHeads[i].email;
+			// if _id doesn't exists that means the user is not in database so create it
+			if (!election.partyHeads[i]._id) {
+				const receivedUser = await this.userService.createIfNotExists(user);
+				election.partyHeads[i]._id = receivedUser._id;
+			}
+		}
+
+		// voters
+		for (let i = 0; i < election.districts.length; i++) {
+			const voterEmails = election.districts[i].tempVoters.trim().split(',');
+			for (let j = 0; j < voterEmails.length; j++) {
+				voterEmails[j] = voterEmails[j].trim();
+				const user = new User();
+				user.email = voterEmails[j];
+
+				// check to see if the user already exists in the voters list
+				let userAlreadyPresent = false;
+				for (let k = 0; k < election.districts[i].voters.length; k++) {
+					if (election.districts[i].voters[k]._id.email === voterEmails[j]) {
+						userAlreadyPresent = true;
+						break;
+					}
+				}
+
+				if (!userAlreadyPresent) {
+					const receivedUser = await this.userService.createIfNotExists(user);
+					const voter = {
+						_id: receivedUser._id,
+						hasVoted: false,
+						votedFor: null
+					};
+					election.districts[i].voters.push(new Voter(voter));
+				}
+			}
+
+			// // delete the deleted ones
+			// let userPresentInNewList = false;
+			// for (let k = 0; k < election.districts[i].voters.length; k++) {
+			// 	for (let j = 0; j < voterEmails.length; j++) {
+			// 		if (election.districts[i].voters[k]._id.email === voterEmails[j]) {
+			// 			userPresentInNewList = true;
+			// 			break;
+			// 		}
+			// 	}
+			//
+			// 	if (!userPresentInNewList) {
+			// 		console.log('deleting user from voters array', election.districts[i].voters[k]._id.email);
+			// 		election.districts[i].voters.splice(k, 1);
+			// 		k--;
+			// 	}
+			// }
+		}
+
+		return await election;
+	}
 }
--- Project/client/src/app/services/election/election.ts
+++ Project/client/src/app/services/election/election.ts
@@ -4,11 +4,17 @@ export class Election {
 	winningStrategy: string;
 	dateFrom: Date;
 	dateTo: Date;
+	nominationCloseDate: Date;
+	previousRound: string;
 	isDistrictElections: boolean;
 	numberOfDistricts: number;
 	candidatesStrategy: string;
-	candidates: Candidate[];
-	voters: Voter[];
+	resultsStrategy: string;
+	resultsReleased: boolean;
+	resultsReleaseDate: Date;
+	electionFrozen: boolean;
+	partyHeads: PartyHead[];
+	districts: District[];
 
 	constructor(election: Election = {} as Election) {
 		this._id = election._id;
@@ -16,33 +22,74 @@ export class Election {
 		this.winningStrategy = election.winningStrategy ? election.winningStrategy : '';
 		this.dateFrom = new Date(election.dateFrom);
 		this.dateTo = new Date(election.dateTo);
+		this.nominationCloseDate = new Date(election.nominationCloseDate);
+		this.previousRound = election.previousRound;
+		this.resultsStrategy = election.resultsStrategy;
+		this.resultsReleased = election.resultsReleased;
+		this.resultsReleaseDate = election.resultsReleaseDate;
+		this.electionFrozen = election.electionFrozen;
 		this.isDistrictElections = election.isDistrictElections ? election.isDistrictElections : false;
 		this.numberOfDistricts = election.numberOfDistricts ? election.numberOfDistricts : 0;
 		this.candidatesStrategy = election.candidatesStrategy ? election.candidatesStrategy : '';
-		this.candidates = election.candidates ? election.candidates : [];
-		this.voters = election.voters ? election.voters : [];
+		this.partyHeads = election.partyHeads ? election.partyHeads : [];
+		this.districts = election.districts ? election.districts : [];
+	}
+}
+
+export class PartyHead {
+	_id: any;
+	email: string;
+	partyName: string;
+
+	constructor(partyHead: PartyHead = {} as PartyHead) {
+		this._id = partyHead._id;
+		this.email = partyHead.email;
+		this.partyName = partyHead.partyName;
+	}
+}
+
+export class District {
+	_id: string;
+	name: string;
+	latitude: number;
+	longitude: number;
+	helpDocumentHTML: string;
+	districtFrozen: boolean;
+	candidates: Candidate[];
+	tempVoters: string;
+	voters: Voter[];
+
+	constructor(district: District = {} as District) {
+		this._id = district._id;
+		this.name = district.name;
+		this.latitude = district.latitude;
+		this.longitude = district.longitude;
+		this.helpDocumentHTML = district.helpDocumentHTML;
+		this.districtFrozen = district.districtFrozen;
+		this.candidates = district.candidates ? district.candidates : [];
+		this.voters = district.voters ? district.voters : [];
 	}
 }
 
 export class Candidate {
-	candidateId: any;
+	_id: any;
 	numOfVotes: number;
 	isApproved: string;
 
 	constructor(candidate: Candidate = {} as Candidate) {
-		this.candidateId = candidate.candidateId;
+		this._id = candidate._id;
 		this.numOfVotes = candidate.numOfVotes;
 		this.isApproved = candidate.isApproved;
 	}
 }
 
 export class Voter {
-	voterId: any;
+	_id: any;
 	hasVoted: boolean;
 	votedFor: string;
 
 	constructor(voter: Voter = {} as Voter) {
-		this.voterId = voter.voterId;
+		this._id = voter._id;
 		this.hasVoted = voter.hasVoted;
 		this.votedFor = voter.votedFor;
 	}
